9528a8f2752e0782b1cd2d5978a5837c0f614a6f
Michael Bernstein <msbernst@mit.edu>
2011-02-21 17:21:59 -0500

    first attack at retainer model

---
+++ retainer.js
@@ -0,0 +1,19 @@
+var showTime = null;
+
+function retainerHide() {
+    console.log("hiding");
+    $('#task-paragraph').hide()
+    .after("<div id='retainer' class='task'>In thirty seconds or less, a paragraph will appear here. Act as quickly as you can when it appears to select all the verbs.<br/><br/><img src='media/preview.png' /></div>");
+    $('#donebtn').attr("disabled", "true").html("HIT will be submittable after job appears");
+}
+
+function setShowCallback() {
+    var waitTime = Math.floor(Math.random()*30) * 1000;
+    console.log("wait time: " + waitTime);
+    window.setTimeout( function() {
+        $('#donebtn').attr("disabled", "").html("Done");    
+        $('#retainer').hide();
+        $('#task-paragraph').show().effect('highlight', {}, 3000);
+        showTime = getServerTime();
+    }, waitTime);
+}
