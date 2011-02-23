33e7db3ca1713d102c0ad09d72183426a41c732f
Michael Bernstein <msbernst@mit.edu>
2011-02-23 12:00:57 -0500

    first sort-of-working prototype of regular testing

--- gettext.cgi
+++ gettext.cgi
@@ -6,36 +6,43 @@ import json
 import nltk
 cgitb.enable()    
 
-print "Content-Type: application/json"     # HTML is following
-print                               # blank line, end of headers
-
-db=MySQLdb.connect(host="mysql.csail.mit.edu", passwd="gangsta2125", user="realtime", db="wordclicker", use_unicode=True)
-cur = db.cursor(MySQLdb.cursors.DictCursor)
-
-form = cgi.FieldStorage()
-id = int(form['textid'].value)
-#print id
+def getText():
+    print "Content-Type: application/json"     # HTML is following
+    print                               # blank line, end of headers
     
-cur.execute("""SELECT text FROM texts WHERE pk = %s""", (id,))
-
-text = cur.fetchone()
-#print text['text'].encode('ascii', 'replace')
-tokenized = nltk.tokenize.word_tokenize(text['text'])
-
-spanned = []
-for index, token in enumerate(tokenized):
-#    print token.encode('ascii', 'replace') + " " + str(type(token))
-    span = u"<span id='word" + unicode(index) +  u"' class='word'>" + token + u"</span>"
-    spanned.append(span)
-
-paragraph = u"<p>" + u" ".join(spanned) + u"</p>"
-#print paragraph.encode('ascii', 'replace')
-
-result = dict()
-result['pk'] = id
-result['text'] = paragraph
-
-print(json.dumps(result))
+    db=MySQLdb.connect(host="mysql.csail.mit.edu", passwd="gangsta2125", user="realtime", db="wordclicker", use_unicode=True)
+    cur = db.cursor(MySQLdb.cursors.DictCursor)
     
-cur.close()
-db.close()
+    form = cgi.FieldStorage()
+    id = int(form['textid'].value)
+    #print id
+        
+    cur.execute("""SELECT text FROM texts WHERE pk = %s""", (id,))
+    
+    text = cur.fetchone()
+    paragraph = htmlizeText(text['text'])
+    
+    result = dict()
+    result['pk'] = id
+    result['text'] = paragraph
+    
+    print(json.dumps(result))
+        
+    cur.close()
+    db.close()
+
+def htmlizeText(text):
+    #print text.encode('ascii', 'replace')
+    tokenized = nltk.tokenize.word_tokenize(text)
+    
+    spanned = []
+    for index, token in enumerate(tokenized):
+    #    print token.encode('ascii', 'replace') + " " + str(type(token))
+        span = u"<span id='word" + unicode(index) +  u"' class='word'>" + token + u"</span>"
+        spanned.append(span)
+    
+    paragraph = u"<p>" + u" ".join(spanned) + u"</p>"
+    #print paragraph.encode('ascii', 'replace')
+    return paragraph
+    
+getText()
--- retainer.js
+++ retainer.js
@@ -1,23 +1,77 @@
 var showTime = null;
+var showTimeout;
+var checkInterval;
 
+// Hides the text from the user
 function retainerHide() {
     console.log("hiding");
     $('#task-paragraph').hide()
     .after("<div id='retainer' class='task'>In thirty seconds or less, a paragraph will appear here. Act as quickly as you can when it appears to select all the verbs.<br/><br/><img src='media/preview.png' /></div>");
+    
     $('#donebtn').attr("disabled", "true").html("HIT will be submittable after job appears");
 }
 
-function setShowCallback() {
-    var waitTime = Math.floor(Math.random()*30) * 1000;
+// Sets a callback to fire and show the text to the user
+function setRetainerCallback() {
+    if (showTime == null) { 
+        //var waitTime = Math.floor(Math.random()*30) * 1000;
+        var waitTime = 30 * 1000; 
+    } else {
+        var waitTime = showTime - getServerTime();
+        console.log("presetting wait time");
+    }
     console.log("wait time: " + waitTime);
-    window.setTimeout( function() {
-        $('#donebtn').attr("disabled", "").html("Done");    
-        $('#retainer').hide();
-        $('#task-paragraph').show().effect('highlight', {}, 3000);
-        showTime = getServerTime();
-    }, waitTime);
+    window.setTimeout(showText, waitTime);
     
+    /*
     window.setTimeout( function() {
         $('#retainer').html('<div style="font-size: 30pt; color:red;">5 seconds left</div>').effect('shake', { times: 5 }, 300);
     }, waitTime - 5000);
+    */
+}
+
+// Sets a specific textid if we are near the regular firing period,
+// and sets the firing time to be exactly on the dot of the firing period
+function scheduleRetainer() {
+    if (assignmentid == 0) {
+        // don't do this in preview mode
+        return;
+    }
+    checkInterval = window.setInterval(function() {
+        $.get('test-timer.cgi?workerid=' + workerid + '&experimentid=' + experiment + '&textid=' + TEST_TEXT_ID, function(data) {
+            if (data['test']) {
+                window.clearInterval(checkInterval);
+                window.clearTimeout(showTimeout);
+                textid = TEST_TEXT_ID;
+                insertText(data);
+                console.log("TEST TIME")
+                showText();
+            } else {
+                console.log("not test time")
+            }
+        });
+    }, 1000);
+    /*
+    if (retainerType == "5min") {
+        var minToFire = 1;
+        
+        var curTime = getServerTime();
+        var modTime = curTime.getTime() % (60 * 1000 * minToFire);
+        console.log("mod time is " + modTime);
+        if (modTime < (30 * 1000)) {   // the random call can only go up to 29 secs
+            showTime = curTime.add((30*1000) - modTime).milliseconds()
+            console.log("fire at " + showTime);
+        }
+    }
+    */
+}
+
+// Shows the text to the user
+function showText() {
+    if (assignmentid != 0) {
+        $('#donebtn').attr("disabled", "").html("Done");    
+    }
+    $('#retainer').hide();
+    $('#task-paragraph').show().effect('highlight', {}, 3000);
+    showTime = getServerTime();
 }
