87f8860eff8f5fbb59f46b9645aaf80f21e5d1d0
Michael Bernstein <msbernst@mit.edu>
2011-03-02 20:15:16 -0500

    fixed bug in parseresults.py

--- quik/quikturkit.js
+++ quik/quikturkit.js
@@ -2,8 +2,9 @@ print("___________________________________");
 
 
 // URL containing the page you want turkers to work on.
+var experimentNum = 15;
 var server = 'flock.csail.mit.edu';
-var url = "http://" + server + "/msbernst/static/word_clicker.html?experiment=14&retainer=1";
+var url = "http://" + server + "/msbernst/static/word_clicker.html?experiment=" + experimentNum + "&retainer=1";
 
 var gettask_url = "http://people.csail.mit.edu/jbigham/locateit/gettask.php?poll=true&qtype=question";
 
