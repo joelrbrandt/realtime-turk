666ae8f754ccb5adee966de4ec0fbcd1fe46869a
Joel Brandt <joelrbrandt@gmail.com>
2011-03-15 12:02:17 -0400

    added alert-sound code. Still needs to be improverated for looping and playing during alert dialogs

---
+++ alert-sound/alert/alert.js
@@ -0,0 +1,29 @@
+soundManager.url = 'alert/swf/'; // directory where SM2 .SWFs live
+
+// Beta-ish HTML5 audio support (force-enabled for iPad), flash-free sound for Safari + Chrome. Enable if you want to try it!
+// soundManager.useHTML5Audio = true;
+
+// do this to skip flash block handling for now. See the flashblock demo when you want to start getting fancy.
+soundManager.useFlashBlock = false;
+
+// disable debug mode after development/testing..
+// soundManager.debugMode = false;
+
+soundManager.onready(function() {
+
+  // SM2 has loaded - now you can create and play sounds!
+    var mySound = soundManager.createSound({
+	id: 'alert-sound',
+	url: 'alert/alert.mp3'
+	// onload: [ event handler function object ],
+    // other options here..
+    });
+    
+
+});
+
+soundManager.ontimeout(function() {
+
+    // (Optional) Hrmm, SM2 could not start. Show an error, etc.?
+
+});
