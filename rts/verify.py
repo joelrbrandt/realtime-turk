e4a5e8dfc3eace918019a03d6e364dfcb448866e
Michael Bernstein <msbernst@mit.edu>
2011-03-17 17:09:28 -0400

    precision/recall values now returning correctly

--- rts/verify.py
+++ rts/verify.py
@@ -10,5 +10,18 @@ def verify(request):
     text_id = int(form['textid'].value)
     verbs = [int(field.value) for field in form.getlist('verbs[]')]
     
-    result = { 'precision': 0.70, 'recall': 0.70 }
+    result = calculateAccuracy(text_id, verbs)
     request.write(json.dumps(result))
+    
+def calculateAccuracy(text_id, verbs):
+    """ Looks up the ground truth in the database and calculates precision and recall. """
+    db=MySQLdb.connect(host=settings.DB_HOST, passwd=settings.DB_PASSWORD, user=settings.DB_USER, db=settings.DB_DATABASE, use_unicode=True)
+    cur = db.cursor()
+    
+    cur.execute("""SELECT wordid FROM groundtruth WHERE textid = %s""", (text_id))
+    ground_truth = [row[0] for row in cur.fetchall()]
+    
+    precision = float(len(set(verbs).intersection(ground_truth))) / len(verbs)
+    recall = float(len(set(verbs).intersection(ground_truth))) / len(ground_truth)
+    return { 'precision': precision, 'recall': recall }
+    
