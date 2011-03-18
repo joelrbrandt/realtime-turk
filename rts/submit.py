a70215c240908c94fb8fcb98932dff7eee1d3e33
Joel Brandt <joelrbrandt@gmail.com>
2011-03-18 17:27:05 -0400

    added amissing question mark in the mturk externalSubmit url

--- rts/submit.py
+++ rts/submit.py
@@ -7,10 +7,13 @@ import rts_logging
 import logging
 
 BASE_HIT_SUBMIT_URL = "http://planetexpress.stanford.edu/foo.php?"
-#BASE_HIT_SUBMIT_URL = "http://www.mturk.com/mturk/externalSubmit"
+#BASE_HIT_SUBMIT_URL = "http://www.mturk.com/mturk/externalSubmit?"
 
 def record_and_redirect(request):
     query = request.parsed_uri[apache.URI_QUERY]
     logging.debug("got a submit request with query string = " + str(query))
     redirect_url = BASE_HIT_SUBMIT_URL + query
     util.redirect(request, redirect_url)
+
+def log_submission_in_db(request):
+    pass
