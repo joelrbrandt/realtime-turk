87df75d6fe997c6501f4a3fd2cb023c02b755d39
Michael Bernstein <msbernst@mit.edu>
2011-03-22 14:43:25 -0400

    can now reliably tell whether worker has open HITs

--- rtsutils/is_worker_busy.py
+++ rtsutils/is_worker_busy.py
@@ -1,56 +1,29 @@
 import settings
-import MySQLdb
+from rtsutils.db_connection import DBConnection
+from datetime import datetime, timedelta
 
 #TODO: time out open assignments after 60 minutes anyway, as a precaution
 
 def isWorkerBusy(worker_id):    
     open_hits = getWorkerOpenHITs(worker_id)
     return len(open_hits) > 0
-
+    
 def getWorkerOpenHITs(worker_id):
-    db=MySQLdb.connect(host=settings.DB_HOST, passwd=settings.DB_PASSWORD, user=settings.DB_USER, db=settings.DB_DATABASE, use_unicode=True)
-    cur = db.cursor(MySQLdb.cursors.DictCursor)
-    
-    open_hits = getAcceptedHITs(worker_id, cur)
-    open_and_alive_hits = filter(lambda x: isExpired(x, cur), open_hits)
-    
-    cur.close()
-    db.close()
-
-    return open_and_alive_hits    
-
-    
-def getAcceptedHITs(worker_id, cur):
     """Returns a list of all HITs where the last word was that the worker had accepted it"""
     open_hits = []
 
     # this gives us the last notification we got for any HIT that this worker touched
-    cur.execute("""SELECT eventtype, notifications.assignmentid, hitid
-    FROM notifications
-    RIGHT JOIN logging ON notifications.assignmentid = logging.assignmentid
-    WHERE logging.workerid =  %s
-    AND logging.event =  'accept'
-    AND TIMESTAMP = ( 
-    SELECT MAX( TIMESTAMP ) 
-    FROM notifications n2
-    WHERE n2.assignmentid = notifications.assignmentid ) 
-    GROUP BY notifications.assignmentid""", (worker_id,) )    
+    db = DBConnection()
+    results = db.query_and_return_array("""SELECT `assignment_duration`, `accept`, `return`, `submit`, `assignmentid` FROM `hits`, `assignments` WHERE `assignments`.hitid = `hits`.hitid AND `assignments`.workerid   = %s """, (worker_id,) )
     
-    for row in cur.fetchall():
-        # if they submitted or returned the HIT, it's not active any more
-        if row['eventtype'] == 'AssignmentSubmitted' or row['eventtype'] == 'AssignmentReturned':
-            continue
+    for row in results:
+        is_done = row['submit'] is not None
+        is_returned = row['return'] is not None
         
-        if row['eventtype'] == 'AssignmentAccepted':
-            # if the last message was an accept, we should be worried            
-            open_hits.append(row)
+        assignment_elapses = datetime.fromtimestamp(row['accept']) + timedelta(seconds = row['assignment_duration'])
+        is_expired = (datetime.now() > assignment_elapses)
+        
+        if not is_done and not is_returned and not is_expired:
+            open_hits.append(row['assignmentid'])
     
     return open_hits
-    
-def isExpired(hit, cur):
-    """ Removes any HITs that have expired or become otherwise reviewable """
-    cur.execute("""SELECT eventtype, max(timestamp), hitid FROM notifications WHERE hitid = %s""", (hit['hitid'], ))
-    result = cur.fetchone()
-    
-    # filter it out if the HIT has expired
-    return result['eventtype'] in ['HITExpired', 'HITReviewable']
