a1bae95dbbfdb75d3d98dd4c4bf3a116f609d552
Joel Brandt <joelrbrandt@gmail.com>
2011-03-24 10:35:15 -0400

    added option in poster to set expiration time of hit

--- rtsutils/word_clicker_poster.py
+++ rtsutils/word_clicker_poster.py
@@ -24,6 +24,8 @@ if __name__ == "__main__":
     parser.add_option("-n", "--number-of-hits", dest="number_of_hits", help="NUMBER of hits", metavar="NUMBER")
     parser.add_option("-b", "--wait-bucket", dest="waitbucket", help="number of SECONDS to wait on retainer", metavar="SECONDS")
     parser.add_option("-p", "--price", dest="price", help="number of CENTS to pay", metavar="CENTS")
+    parser.add_option("-x", "--expiration-time", dest="expiration", help="number of seconds before hit EXPIRES", metavar="EXPIRES")
+
 
     (options, args) = parser.parse_args()
 
@@ -31,6 +33,7 @@ if __name__ == "__main__":
     b = int(options.waitbucket)
     e = int(options.experiment_number)
     p = int(options.price)/100.0
+    x = int(options.expiration)
 
 
     c = get_mt_conn()
@@ -39,7 +42,7 @@ if __name__ == "__main__":
                        waitbucket=b,
                        reward_as_usd_float=p,
                        assignment_duration=b+120,
-                       lifetime=10*60)
+                       lifetime=x)
 
     for i in range(n):
         try:
