6500b44aa3b8386a99e04b55972a703d454d4bad
Joel Brandt <joelrbrandt@gmail.com>
2011-03-26 10:38:18 -0400

    ip geocoding

--- rtsutils/settings.template.py
+++ rtsutils/settings.template.py
@@ -16,9 +16,21 @@ DB_PASSWORD = 'password'
 DB_HOST = 'localhost'
 DB_DATABASE = 'dbname'
 
+# Elevated user has all db priviledges -- DO NOT use for web code
+DB_ELEVATED_USER = 'username-elev'
+DB_ELEVATED_PASSWORD = 'password-elev'
+
 #
 # External HIT server info
 #
 
 HIT_SERVER = "flock.csail.mit.edu"
 HIT_SERVER_USER_DIR = '[your username]'
+
+#
+# GeoIP settings
+#
+# Data file available from: http://www.maxmind.com/app/geolitecity
+#
+
+GEOIP_DATA_FILE_LOCATION = "/var/local/geoip/GeoLiteCity.dat"
