0c44eb98043182c2a63356015e412e4acdc914ae
Michael Bernstein <msbernst@mit.edu>
2011-04-15 01:15:02 -0400

    adding photographer labeling code

---
+++ rts/video/slow_submit.py
@@ -0,0 +1,32 @@
+from mod_python import apache, util
+import simplejson
+
+from rts import rts_logging
+import logging
+from rtsutils.db_connection import DBConnection
+
+import submit
+
+def slowSubmit(request):
+    """ Handles submissions from the "slow" server """
+
+    form = util.FieldStorage(request)
+    # Get relevant items out of the form
+    assignmentid = submit.get_value_or_none(form, "assignmentId")
+    workerid = submit.get_value_or_none(form, "w")    
+    snapshots_raw = submit.get_value_or_none(form, "sn")
+
+    snapshots = simplejson.loads(snapshots_raw)
+    logging.debug("SNAPSHOTS: %s" % snapshots)    
+
+    db = DBConnection()
+    
+    for snapshot in snapshots:
+        db.query_and_return_array("""INSERT INTO slow_snapshots (assignmentid, location) VALUES (%s, %s)""", (assignmentid, snapshot) )
+
+    if workerid == "photographer":
+        # photographer doesn't submit a HIT :)
+        submit.log_submission_in_db(request)
+        request.write("OK, got it! %s for %s" % (snapshots, assignmentid))
+    else:
+        submit.record_and_redirect(request)
