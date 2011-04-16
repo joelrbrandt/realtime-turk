9c9f57605e6210087aa401c819ec7dc8c9cd5daa
Michael Bernstein <msbernst@mit.edu>
2011-04-16 19:47:16 -0400

    voting

--- rtsutils/get_photo.py
+++ rtsutils/get_photo.py
@@ -14,3 +14,15 @@ def getPhotos(filename):
     filenumbers = ['%03d' % min(int(row['location'] * 100 + 1), 100) for row in locations]
     
     return [VIDEO_HOST_DIRECTORY + filename + filenumber + '.jpg' for filenumber in filenumbers]
+    
+def getSlowPhotos(videoid, limit):
+    db = DBConnection()
+    locations = db.query_and_return_array("""SELECT filename, location FROM slow_snapshots, assignments, videos WHERE slow_snapshots.assignmentid = assignments.assignmentid AND videos.pk = assignments.videoid AND assignments.videoid = %s ORDER BY slow_snapshots.pk ASC LIMIT %s""", (videoid, limit))
+    
+    filename = locations[0]['filename']
+    
+    # don't forget off-by-one: slider starts at 0, but first photo is photo1.jpg
+    filenumbers = set(['%03d' % min(int(row['location'] * 100 + 1), 100) for row in locations]) # use set() so we don't get copies
+    
+    return [VIDEO_HOST_DIRECTORY + filename + filenumber + '.jpg' for filenumber in filenumbers]
+    
