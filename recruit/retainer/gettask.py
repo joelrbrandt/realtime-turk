ac789ee7ee84f0204097901d946eec24b778e5a1
Michael Bernstein <msbernst@mit.edu>
2011-12-21 12:20:11 -0500

    gettask is working

--- recruit/retainer/gettask.py
+++ recruit/retainer/gettask.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+import json
 from retainer.models import *
 
 from datetime import datetime, timedelta
@@ -17,26 +18,74 @@ PING_TYPES = {
 def get_task(request, worker_id, assignment_id):
     # find out whether the worker has already been assigned a task. if so, just return that task.
     current_assignments = Assignments.objects.filter(assignment_id = assignment_id)
-    if len(current_assignments) == 1:
-        return get_task_info(current_assignments[0].task_id)
+    if len(current_assignments) > 0 and current_assignments[0].task_id is not None:
+        task_info = get_task_info(current_assignments[0].task_id)
+    else:
+        # are there enough workers waiting?
+        # if so, ask the application server to give us a task to do, or to decline giving a task
+        waiting_assignments = waiting_workers()
+        if len(waiting_assignments) >= MIN_WAITING_WORKERS:
+            worker_map = get_assignment_task_map(waiting_assignments)
+            save_assignment_task_map(worker_map)
+        
+            # now we get the current worker's task out of that map and return it to the client
+            current_worker_task = waiting_assignments[assignment_id]
+            task_info = get_task_info(current_worker_task)
+        else:
+            # not enough workers, return no job
+            task_info = { 'task_id': None }
 
-    # are there enough workers waiting?
-    # if so, ask the application server to give us a task to do, or to decline giving a task
-    num_waiting = num_waiting_workers()
-
-    return HttpResponse(u'HI ' + worker_id)
-
-
-# Given a task id, return a json object that describes that task
-def get_task_info(task_id):
-    return HttpResponse(u'There is a task and it is awesome: ' + task_id)
+    # now turn the task_info python into JSON to return to the client
+    serialized = json.dumps(task_info)
+    return HttpResponse(serialized, mimetype="application/json")
 
 
 # Return the number of workers who are waiting on retainer right now
-def num_waiting_workers():
+def waiting_workers():
     time_now = unixtime(datetime.now())
     time_ping_floor = unixtime(datetime.now() - timedelta(seconds = PING_TIMEOUT_SECONDS))
     print(time_now)
     print(time_ping_floor)
 
-    #recent_pings = Events.objects.filter(event_type = PING_TYPES['waiting'])
+    recent_pings = Assignments.objects.filter(events__event_type = 'ping-waiting', events__server_time__gte = 0)
+    #recent_workers = recent_pings.values('worker_id').distinct()
+    #worker_ids = [worker['worker_id'] for worker in recent_
+
+    return recent_pings
+
+
+def save_assignment_task_map(assignment_map):
+    for assignment_id in assignment_map.keys():
+        task_id = assignment_map[assignment_id]
+        assignment = Assignments.objects.get(assignment_id = assignment_id)
+        assignment.task_id = task_id
+        assignment.save()
+
+
+
+
+
+
+# ~~~~~~~~~~~~~~ APPLICATION SERVER CODE ~~~~~~~~~~~~~~~~~~~
+
+
+# Asks the server to return task mapping for the set of workers we provide,
+# or return None if the workers fail to meet any additional qualifications
+# or if there is no task ready to be completed
+def get_assignment_task_map(waiting_assignments):
+    # TODO: this should be moved into a separate module or become part of the application server
+    return {
+        # assignmentid : taskid
+        u'1': 1
+        #u'2': 1
+    }
+
+
+# Given a task id, return a json object that describes that task
+def get_task_info(task_id):
+    return {
+        'task_id': 1,
+        'photo_url': 'http://www.google.com',
+    }
+
+
