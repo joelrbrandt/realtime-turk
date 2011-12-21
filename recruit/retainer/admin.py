78f66a9c821662707ceb46dc0bd0d732b840ab4b
Michael Bernstein <msbernst@mit.edu>
2011-12-21 12:20:11 -0500

    gettask is working

---
+++ recruit/retainer/admin.py
@@ -0,0 +1,7 @@
+from retainer.models import *
+from django.contrib import admin
+
+admin.site.register(Assignments)
+admin.site.register(Hits)
+admin.site.register(Events)
+admin.site.register(Notifications)
